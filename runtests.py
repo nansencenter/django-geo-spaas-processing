3f19a5d6c518fac25173ac3978733da776824c4e
aanersc <arash.azamifard@nersc.no>
2020-10-23 14:30:57 +0200

    #15 minors

--- runtests.py
+++ runtests.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     test_module = f".{sys.argv[1]}" if len(sys.argv) >= 2 else ''
 
     TestRunner = get_runner(django.conf.settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(interactive=False)
     failures = test_runner.run_tests(["tests" + test_module])
     sys.exit(bool(failures))

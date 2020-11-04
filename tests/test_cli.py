ba9b270e703adec222e5d30e04f08c22102a686d
aanersc <arash.azamifard@nersc.no>
2020-11-04 08:45:56 +0100

    #19 minors in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,32 +21,33 @@ class DownlaodingCLITestCase(unittest.TestCase):
             '-b', "200",
             '-e', "2020-08-22",
             '-r',
-            '-n',"100",
+            '-s', "100",
             '-p',
             '-g', "POLYGON ((-22 84, -22 74, 32 74, 32 84, -22 84))",
             '-c', "/config_folder/config_file.yml",
             '-q',
-            '{"dataseturi__uri__contains": "osisaf", "source__instrument__short_name__icontains": "AMSR2"}',
+            '{"dataseturi__uri__contains": "osisaf", "source__instrument__short_name__icontains": '
+            + '"AMSR2"}',
         ]
 
     def test_extract_arg(self):
         """shall return the correct argument values based on the 'sys.argv' """
-        arg = cli_download.parse_args()
+        arg = cli_download.cli_parse_args()
         self.assertEqual(arg.begin, '200')
         self.assertEqual(arg.config_file, '/config_folder/config_file.yml')
         self.assertEqual(arg.down_dir, '/test_folder/%Y_nh_polstere')
         self.assertEqual(arg.end, '2020-08-22')
         self.assertEqual(arg.geometry, 'POLYGON ((-22 84, -22 74, 32 74, 32 84, -22 84))')
-        self.assertEqual(arg.number, '100')
-        self.assertEqual(
-            arg.query,
-            '{"dataseturi__uri__contains": "osisaf", "source__instrument__short_name__icontains": "AMSR2"}')
+        self.assertEqual(arg.safety_limit, '100')
+        self.assertEqual(arg.query,
+                         '{"dataseturi__uri__contains": "osisaf", '
+                         +'"source__instrument__short_name__icontains": "AMSR2"}')
         # testing the flag enumeration
         self.assertTrue(arg.rel_time_flag)
         self.assertTrue(arg.use_filename_prefix)
         sys.argv.remove('-r')
         sys.argv.remove('-p')
-        arg = cli_download.parse_args()
+        arg = cli_download.cli_parse_args()
         self.assertFalse(arg.rel_time_flag)
         self.assertFalse(arg.use_filename_prefix)
 
@@ -54,9 +55,9 @@ class DownlaodingCLITestCase(unittest.TestCase):
     @mock.patch('geospaas_processing.downloaders.DownloadManager.download')
     def test_correct_call_json_deserializer(self, mock_download_method, mock_download_manager_init):
         """'json.loads' shall deserialize the whole string that comes after '-q' """
-        arg = cli_download.parse_args()
+        arg = cli_download.cli_parse_args()
         with mock.patch('json.loads') as mock_json:
-            cli_download.main(arg)
+            cli_download.main()
         self.assertIn(
             ('{"dataseturi__uri__contains": "osisaf", '
              '"source__instrument__short_name__icontains": "AMSR2"}',),
@@ -65,12 +66,12 @@ class DownlaodingCLITestCase(unittest.TestCase):
     @mock.patch('geospaas_processing.downloaders.DownloadManager.__init__', return_value=None)
     @mock.patch('geospaas_processing.downloaders.DownloadManager.download')
     def test_lack_of_calling_json_deserializer_when_no_query_appears(
-        self, mock_download_method, mock_download_manager_init):
+            self, mock_download_method, mock_download_manager_init):
         """'json.loads' should not called when nothing comes after '-q' """
-        arg = cli_download.parse_args()
-        arg.query=""
+        sys.argv.pop()
+        sys.argv.pop()
         with mock.patch('json.loads') as mock_json:
-            cli_download.main(arg)
+            cli_download.main()
         self.assertIsNone(mock_json.call_args)
 
     @mock.patch('geospaas_processing.downloaders.DownloadManager.__init__', return_value=None)
@@ -81,8 +82,8 @@ class DownlaodingCLITestCase(unittest.TestCase):
         sys.argv.remove('-p')
         sys.argv.remove('-r')
         sys.argv[4] = '2019-10-22'
-        arg = cli_download.parse_args()
-        cli_download.main(arg)
+        arg = cli_download.cli_parse_args()
+        cli_download.main()
         self.assertIn({
             'download_directory': '/test_folder/%Y_nh_polstere',
             'geographic_location__geometry__intersects':
@@ -100,11 +101,13 @@ class DownlaodingCLITestCase(unittest.TestCase):
     @mock.patch('geospaas_processing.downloaders.DownloadManager.download')
     def test_correct_call_DownloadManager_with_file_prefix(
             self, mock_download_method, mock_download_manager_init):
-        "shall return the proper call for the case of lack of two definite time points in arguments"
+        """
+        shall return the proper call for the case of lack of two definite time points in arguments
+        """
         sys.argv.remove('-r')
         sys.argv[4] = '2019-10-22'
-        arg = cli_download.parse_args()
-        cli_download.main(arg)
+        arg = cli_download.cli_parse_args()
+        cli_download.main()
         self.assertIn({
             'download_directory': '/test_folder/%Y_nh_polstere',
             'geographic_location__geometry__intersects':
@@ -122,13 +125,13 @@ class DownlaodingCLITestCase(unittest.TestCase):
     @mock.patch('geospaas_processing.downloaders.DownloadManager.download')
     def test_correct_call_DownloadManager_without_geometry(
             self, mock_download_method, mock_download_manager_init):
-        "shall return the proper call for the case of lack of two definite time points in arguments"
+        """shall return the proper call for the case of lack of geometry in arguments"""
         sys.argv.remove('-g')
         sys.argv.remove("POLYGON ((-22 84, -22 74, 32 74, 32 84, -22 84))")
         sys.argv.remove('-r')
         sys.argv[4] = '2019-10-22'
-        arg = cli_download.parse_args()
-        cli_download.main(arg)
+        arg = cli_download.cli_parse_args()
+        cli_download.main()
         self.assertIn({
             'download_directory': '/test_folder/%Y_nh_polstere',
             'max_downloads': 100,
@@ -144,11 +147,11 @@ class DownlaodingCLITestCase(unittest.TestCase):
     @mock.patch('geospaas_processing.downloaders.DownloadManager.download')
     def test_correct_call_DownloadManager_with_relative_time(
             self, mock_download_method, mock_download_manager_init):
-        "shall return the proper call for the case of lack of relative time definition in arguments"
+        """shall return the proper call for the case of relative time definition in arguments"""
         sys.argv[4] = "40"
-        arg = cli_download.parse_args()
+        arg = cli_download.cli_parse_args()
         with freeze_time("2012-01-14"):
-            cli_download.main(arg)
+            cli_download.main()
         self.assertIn({
             'download_directory': '/test_folder/%Y_nh_polstere',
             'geographic_location__geometry__intersects':
@@ -165,10 +168,10 @@ class DownlaodingCLITestCase(unittest.TestCase):
     def test_find_designated_time_function(self):
         """test the 'find_designated_time' function logics. answer_1, answer_2 are used for absolute
         and answer_3, answer_4 are used for relative timing"""
-        answer_1, answer_2 = cli_download.find_designated_time(False,'2019-10-22','2020-08-22')
-        self.assertEqual(answer_1,datetime(2019, 10, 22, 0, 0, tzinfo=tzutc()))
-        self.assertEqual(answer_2,datetime(2020, 8, 22, 0, 0, tzinfo=tzutc()))
+        answer_1, answer_2 = cli_download.find_designated_time(False, '2019-10-22', '2020-08-22')
+        self.assertEqual(answer_1, datetime(2019, 10, 22, 0, 0, tzinfo=tzutc()))
+        self.assertEqual(answer_2, datetime(2020, 8, 22, 0, 0, tzinfo=tzutc()))
         with freeze_time("2012-01-14"):
-            answer_3, answer_4 = cli_download.find_designated_time(True,'500','')
-            self.assertEqual(answer_3,datetime(2011, 12, 24, 4, 0, tzinfo=tzutc()))
-            self.assertEqual(answer_4,datetime(2012, 1, 14, 0, 0, tzinfo=tzutc()))
+            answer_3, answer_4 = cli_download.find_designated_time(True, '500', '')
+            self.assertEqual(answer_3, datetime(2011, 12, 24, 4, 0, tzinfo=tzutc()))
+            self.assertEqual(answer_4, datetime(2012, 1, 14, 0, 0, tzinfo=tzutc()))

dbc238e9eec541fd097ccca271f6c11c4190140b
aanersc <arash.azamifard@nersc.no>
2020-11-09 15:57:53 +0100

    #21 minors in download.py

--- geospaas_processing/cli/download.py
+++ geospaas_processing/cli/download.py
@@ -30,7 +30,7 @@ def main():
         provider_settings_path=arg.config_file,
         max_downloads=int(arg.safety_limit),
         use_file_prefix=arg.use_filename_prefix,
-        store_address=arg.address_storage,
+        save_path=arg.save_path,
         time_coverage_start__gte=designated_begin,
         time_coverage_end__lte=designated_end,
         **cumulative_query
@@ -81,9 +81,8 @@ def cli_parse_args():
         help="The flag that distinguishes between the two cases of having files WITH or WITHOUT "
         + "file prefix when downloaded")
     parser.add_argument(
-        '-a', '--address_storage', action='store_true',
-        help="The flag that distinguishes between the two cases of whether storing the local "
-        + "address of file in the dataset or not by its ABSENCE or PRESENCE in the arguments.")
+        '-a', '--save_path', action='store_true',
+        help="Save path to local file in the database based on its ABSENCE or PRESENCE.")
     parser.add_argument(
         '-g', '--geometry', required=False, type=str,
         help="The 'wkt' string of geometry which is acceptable by 'GEOSGeometry' of django")

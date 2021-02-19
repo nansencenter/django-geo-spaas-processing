dcb6c8e8b60d5be34fecd5b2bdefb759e5136ce9
Adrien Perrin <adrien.perrin@nersc.no>
2021-02-19 14:39:17 +0000

    add FTP downloader and remove file prefixes

--- geospaas_processing/cli/download.py
+++ geospaas_processing/cli/download.py
@@ -27,12 +27,10 @@ def main():
         download_directory=arg.destination_path.rstrip(os.path.sep),
         provider_settings_path=arg.config_file,
         max_downloads=int(arg.safety_limit),
-        use_file_prefix=arg.use_filename_prefix,
         save_path=arg.save_path,
         **cumulative_query
     )
     download_manager.download()
-
 
 
 def cli_parse_args():
@@ -43,10 +41,6 @@ def cli_parse_args():
         help="The upper limit (safety limit) of number of datasets that are going to be downloaded."
         + " If there total number of requested dataset for downloading exceeds this number, the "
         + "downloading process does not commence.")
-    parser.add_argument(
-        '-p', '--use_filename_prefix', action='store_true',
-        help="The flag that distinguishes between the two cases of having files WITH or WITHOUT "
-        + "file prefix when downloaded")
     parser.add_argument(
         '-a', '--save_path', action='store_true',
         help="Save path to local file in the database based on its ABSENCE or PRESENCE.")

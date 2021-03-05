f7234c225fc237765261b1265124a2dac6073a0e
azamifard <arash.azamifard@nersc.no>
2021-03-05 14:12:04 +0100

    #41 minors in script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-IMAGE_ARRAY=('IMAGE_NAME_WORKER' 'IMAGE_NAME_CLI')
 for tag in $*;do
-    for IMAGE in "${IMAGE_ARRAY[@]}";do
+    for IMAGE in "$IMAGE_NAME_WORKER" "$IMAGE_NAME_WORKER";do
         echo "Tag ${IMAGE}:${DOCKER_TMP_TAG} as ${IMAGE}:${tag}"
         docker tag "${IMAGE}:${DOCKER_TMP_TAG}" "${IMAGE}:${tag}"
         echo "Push ${IMAGE}:${tag}"

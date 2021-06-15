862d7c36421a720a7beb7f8f892fdc71843012f8
Adrien Perrin <adrien.perrin@nersc.no>
2021-06-15 14:57:55 +0200

    remove travis config

--- scripts/docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for IMAGE in "$IMAGE_NAME_WORKER" "$IMAGE_NAME_CLI";do
-    for tag in $*;do
-        echo "Tag ${IMAGE}:${DOCKER_TMP_TAG} as ${IMAGE}:${tag}"
-        docker tag "${IMAGE}:${DOCKER_TMP_TAG}" "${IMAGE}:${tag}"
-        echo "Push ${IMAGE}:${tag}"
-        docker push "${IMAGE}:${tag}"
-    done
-done

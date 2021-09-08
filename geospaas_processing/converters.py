f9551276b0c271be33cc7ab87e4df78ec3a3efe0
Adrien Perrin <adrien.perrin@nersc.no>
2021-09-08 15:51:56 +0200

    retry the move after removing existing file

--- geospaas_processing/converters.py
+++ geospaas_processing/converters.py
@@ -161,16 +161,18 @@ class IDFConverter():
                 try:
                     shutil.move(tmp_result_path, permanent_collection_dir)
                 except shutil.Error as error:
-                    # if the directory already exists, we don't copy it again
+                    # if the directory already exists, we remove it and
+                    # retry to move the result file
                     if 'already exists' in str(error):
                         existing_dir = os.path.join(permanent_collection_dir, result_dir)
-                        LOGGER.info("%s already exists, removing it", existing_dir)
+                        LOGGER.info("%s already exists, removing it and retrying", existing_dir)
                         if os.path.isdir(existing_dir):
                             shutil.rmtree(existing_dir)
                         elif os.path.isfile(existing_dir):
                             os.remove(existing_dir)
                         else:
                             raise
+                        shutil.move(tmp_result_path, permanent_collection_dir)
                     else:
                         raise
 

148aef499aa647af88df61045fc677d913d69951
Adrien Perrin <adrien.perrin@nersc.no>
2022-11-04 10:39:44 +0100

    conditionally import converters

--- geospaas_processing/converters/__init__.py
+++ geospaas_processing/converters/__init__.py
@@ -1,2 +1,8 @@
-from .idf.converter import IDFConversionManager
-from .syntool.converter import SyntoolConversionManager
+import os
+
+enabled_converters = os.getenv('GEOSPAAS_PROCESSING_ENABLE_CONVERTERS', 'idf,syntool').split(',')
+
+if 'idf' in enabled_converters:
+    from .idf.converter import IDFConversionManager
+if 'syntool' in enabled_converters:
+    from .syntool.converter import SyntoolConversionManager

1d9510c20e6e8b55f6647a6e12df8800981734f6
Adrien Perrin <adrien.perrin@nersc.no>
2022-12-07 11:46:43 +0100

    add ops function for netcdf cropping

--- geospaas_processing/ops.py
+++ geospaas_processing/ops.py
@@ -4,6 +4,8 @@ reprojection, etc.)
 import logging
 from enum import Enum
 
+from nco import Nco, NCOException
+nco = Nco()
 try:
     from osgeo import gdal
 except ImportError:
@@ -20,13 +22,38 @@ class OutputFormat(Enum):
     vrt = 'VRT'
 
 
-def crop(in_file, out_file, bbox, output_format=OutputFormat.geotiff):
+def gdal_crop(in_file, out_file, bbox, output_format=OutputFormat.geotiff):
+    """Cropping function for files without subdatasets. Uses GDAL.
+    """
+    options = gdal.TranslateOptions(
+        projWin=tuple(str(b) for b in bbox[0:4]),
+        format=output_format.value)
+    gdal.Translate(str(out_file), str(in_file), options=options)
+
+
+def nco_crop(in_file, out_file, bbox):
+    """Cropping function for netCDF files. Uses NCO.
+    """
+    try:
+        nco.ncks(
+            input=in_file,
+            output=out_file,
+            options=[
+                f"-d lat,{bbox[0]:f},{bbox[2]:f}",
+                f"-d lon,{bbox[3]:f},{bbox[1]:f}",
+            ])
+    except NCOException as error:
+        raise RuntimeError('An error happened during cropping. Please check that the bounding '
+                           'box is within the datasets spatial coverage') from error
+
+
+def crop(in_file, out_file, bbox):
     """Subset a dataset file using a bounding box.
     bbox is a sequence containing the limits of the bounding box in the
     projection of the dataset in the following order:
     west, north, east, south
     """
-    options = gdal.TranslateOptions(
-        projWin=tuple(str(b) for b in bbox[0:4]),
-        format=output_format.value)
-    return gdal.Translate(str(out_file), str(in_file), options=options)
+    if in_file.endswith('.nc'):
+        nco_crop(in_file, out_file, bbox)
+    else: # untested
+        gdal_crop(in_file, out_file, bbox)

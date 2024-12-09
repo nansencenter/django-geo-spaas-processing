f86e43136e0bdd2a76e0ef913808c54d0d6660aa
Adrien Perrin <adrien.perrin@nersc.no>
2024-12-09 12:49:23 +0000

    split basic syntool converter run() into several methods

--- geospaas_processing/converters/syntool/converter.py
+++ geospaas_processing/converters/syntool/converter.py
@@ -83,7 +83,7 @@ class SyntoolConverter(Converter):
                            process.stdout, process.stderr)
         return results
 
-    def post_ingest(self, results, out_dir, **kwargs):
+    def post_ingest(self, results, out_dir, kwargs):
         """Post-ingestion step, the default is to create a "features"
         directory containing some metadata about what was generated
         """
@@ -235,24 +235,21 @@ class BasicSyntoolConverter(SyntoolConverter):
         converter_args.extend(self.parse_converter_options(kwargs))
         return converter_args
 
-    def run(self, in_file, out_dir, **kwargs):
-        """Transforms a file into a Syntool-displayable format using
-        the syntool-converter and syntool-ingestor tools
-        """
-        results_dir = kwargs.pop('results_dir')
-        # syntool-converter
+    def run_conversion(self, in_file, out_dir, kwargs):
+        """Run the Syntool converter on the input file"""
         if self.converter_type is not None:
             converted_files = self.convert(in_file, out_dir,
                                            self.parse_converter_args(kwargs),
                                            **kwargs)
         else:
             converted_files = (in_file,)
+        return [Path(out_dir, converted_file) for converted_file in converted_files]
 
-        # syntool-ingestor
+    def run_ingestion(self, converted_paths, results_dir, kwargs):
+        """Run the Syntool ingestor on the conversion results"""
         ingestor_config = Path(kwargs.pop('ingestor_config', 'parameters/3413.ini'))
         results = []
-        for converted_file in converted_files:
-            converted_path = Path(out_dir, converted_file)
+        for converted_path in converted_paths:
             for ingest_config in self.find_ingest_config(converted_path):
                 results.extend(self.ingest(
                     converted_path, results_dir, [
@@ -264,8 +261,16 @@ class BasicSyntoolConverter(SyntoolConverter):
                 os.remove(converted_path)
             except IsADirectoryError:
                 shutil.rmtree(converted_path)
+        return results
 
-        self.post_ingest(results, results_dir, **kwargs)
+    def run(self, in_file, out_dir, **kwargs):
+        """Transforms a file into a Syntool-displayable format using
+        the syntool-converter and syntool-ingestor tools
+        """
+        results_dir = kwargs.pop('results_dir')
+        converted_paths = self.run_conversion(in_file, out_dir, kwargs)
+        results = self.run_ingestion(converted_paths, results_dir, kwargs)
+        self.post_ingest(results, results_dir, kwargs)
         return results
 
 

f7362a470611fa6d86c6a279131ef9315de34b51
Adrien Perrin <adrien.perrin@nersc.no>
2025-04-14 13:08:07 +0000

    add "in place" publishing
    Just add a prefix to the task results, the files are not moved.
    The working directory needs to be published by external means.

--- geospaas_processing/tasks/core.py
+++ geospaas_processing/tasks/core.py
@@ -138,45 +138,52 @@ def copy(self, args, copy_to=None):  # pylint: disable=unused-argument
 
 @app.task(base=FaultTolerantTask, bind=True, track_started=True)
 @lock_dataset_files
-def publish(self, args):  # pylint: disable=unused-argument
+def publish(self, args, publish_mode='in_place'):  # pylint: disable=unused-argument
     """Copy the file (tree) located at `args[1]` to the FTP server (using SCP)"""
     dataset_id = args[0]
     dataset_files_paths = args[1] or []
 
-    ftp_host = os.getenv('GEOSPAAS_PROCESSING_FTP_HOST', None)
-    ftp_root = os.getenv('GEOSPAAS_PROCESSING_FTP_ROOT', None)
-    ftp_path = os.getenv('GEOSPAAS_PROCESSING_FTP_PATH', None)
+    if publish_mode == 'in_place':
+        prefix = os.getenv('GEOSPAAS_PROCESSING_PUBLISH_PREFIX', '')
+        results = [
+            f"{prefix.rstrip('/')}/{p}"
+            for p in dataset_files_paths
+        ]
+    elif publish_mode == 'ftp':
+        ftp_host = os.getenv('GEOSPAAS_PROCESSING_FTP_HOST', None)
+        ftp_root = os.getenv('GEOSPAAS_PROCESSING_FTP_ROOT', None)
+        ftp_path = os.getenv('GEOSPAAS_PROCESSING_FTP_PATH', None)
 
-    if not (ftp_host and ftp_root and ftp_path):
-        raise RuntimeError(
-            'The following environment variables should be set: ' +
-            'GEOSPAAS_PROCESSING_FTP_HOST, ' +
-            'GEOSPAAS_PROCESSING_FTP_ROOT, ' +
-            'GEOSPAAS_PROCESSING_FTP_PATH.'
-        )
+        if not (ftp_host and ftp_root and ftp_path):
+            raise RuntimeError(
+                'The following environment variables should be set: ' +
+                'GEOSPAAS_PROCESSING_FTP_HOST, ' +
+                'GEOSPAAS_PROCESSING_FTP_ROOT, ' +
+                'GEOSPAAS_PROCESSING_FTP_PATH.'
+            )
 
-    # It is assumed that the remote server uses "/"" as path separator
-    remote_storage_path = posixpath.join(ftp_root, ftp_path)
-    ftp_storage = utils.RemoteStorage(host=ftp_host, path=remote_storage_path)
+        # It is assumed that the remote server uses "/"" as path separator
+        remote_storage_path = posixpath.join(ftp_root, ftp_path)
+        ftp_storage = utils.RemoteStorage(host=ftp_host, path=remote_storage_path)
 
-    logger.info("Checking if there is enough free space on %s:%s", ftp_host, remote_storage_path)
-    total_size = utils.LocalStorage(path=WORKING_DIRECTORY).get_files_size(dataset_files_paths)
-    ftp_storage.free_space(total_size)
+        logger.info("Checking if there is enough free space on %s:%s", ftp_host, remote_storage_path)
+        total_size = utils.LocalStorage(path=WORKING_DIRECTORY).get_files_size(dataset_files_paths)
+        ftp_storage.free_space(total_size)
 
-    results = []
-    for file in dataset_files_paths:
-        dataset_local_path = os.path.join(WORKING_DIRECTORY, file)
-        logger.info("Copying %s to %s:%s", dataset_local_path, ftp_host,
-                    os.path.join(remote_storage_path, file))
-        try:
-            ftp_storage.put(dataset_local_path, file)
-        except scp.SCPException as error:
-            if 'No space left on device' in str(error):
-                ftp_storage.remove(file)
-                self.retry((args,), countdown=90, max_retries=5)
-            else:
-                raise
-        results.append(f"ftp://{ftp_host}/{ftp_path}/{file}")
+        results = []
+        for file in dataset_files_paths:
+            dataset_local_path = os.path.join(WORKING_DIRECTORY, file)
+            logger.info("Copying %s to %s:%s", dataset_local_path, ftp_host,
+                        os.path.join(remote_storage_path, file))
+            try:
+                ftp_storage.put(dataset_local_path, file)
+            except scp.SCPException as error:
+                if 'No space left on device' in str(error):
+                    ftp_storage.remove(file)
+                    self.retry((args,), countdown=90, max_retries=5)
+                else:
+                    raise
+            results.append(f"ftp://{ftp_host}/{ftp_path}/{file}")
 
     return (dataset_id, results)
 
